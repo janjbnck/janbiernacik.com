89faa7d63f1208795ffe2766ef7e465f1e4ff948
Jan Biernacik <jan@biernacik.dev>
2025-08-06 22:17:50 +0200

    First Preparations

---
+++ src/lib/assets/components/LangChangeButton.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { text } from '@sveltejs/kit';
+	import { getLocaleFromNavigator, locale } from 'svelte-i18n';
+
+	const userDefaultLang: string = String(getLocaleFromNavigator()).slice(0, 2);
+	let langId: number;
+
+	if (userDefaultLang.slice(0, 2) === 'de') {
+		langId = 2;
+	} else {
+		langId = 1;
+	}
+
+	const changeLanguage = () => {
+		if (langId !== 2) {
+			locale.set('de');
+			langId = 2;
+		} else {
+			locale.set('en');
+			langId = 1;
+		}
+	};
+</script>
+
+<button on:click={changeLanguage}>Change Language</button>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import '../app.css';
+	import '$lib/assets/i18n/index.ts';
+	import favicon from '$lib/assets/favicon.svg';
+
+	import { _ } from 'svelte-i18n';
+
+	let { children } = $props();
+</script>
+
+<svelte:head>
+	<title>Jan Biernacik - {$_('pages.homePage.pageTitle')}</title>
+	<link rel="icon" href={favicon} />
+</svelte:head>
+
+{@render children?.()}
