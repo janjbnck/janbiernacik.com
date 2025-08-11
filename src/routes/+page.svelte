b9a9cdb271d5a83a9d612b578c86536e99ead335
Jan Biernacik <jan@biernacik.dev>
2025-08-11 19:36:06 +0200

    Temporary GitHub Redirect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { browser } from '$app/environment';
+
+	if (browser) window.location.href = 'https://github.com/janjbnck';
+</script>
+
+<a href="https://github.com/janjbnck">GitHub</a>
