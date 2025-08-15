9512e21974cbebfb5e72a9c0a5b21c9e5f792ca3
Jan Biernacik <jan@biernacik.dev>
2025-08-15 12:39:50 +0200

    Fix fallback redirect

--- src/routes/de/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import { browser } from '$app/environment';
-
-	if (browser) window.location.href = '/';
-</script>
-
-<svelte:head>
-	<meta name="robots" content="noindex, nofollow" />
-</svelte:head>
