b05af426c880c4956505249173cda1d7bd87220f
Jan Biernacik <jan@biernacik.dev>
2025-08-16 22:11:18 +0200

    Fix header back button design

--- src/lib/assets/components/PageHeader.svelte
+++ src/lib/assets/components/PageHeader.svelte
@@ -5,6 +5,6 @@
 
 <div class="pt-[var(--spacing-x-large)]">
 	<h1>{pageTitle}</h1>
-	<a href="/" class="hover:text-[var(--color-text)] no-underline"><i class="bi bi-arrow-left-circle-fill"></i> {$t('specialMsg.goBack')}</a>
+	<a href="/" class="hover:text-[var(--color-text)]! no-underline!"><i class="bi bi-arrow-left-circle-fill"></i> {$t('specialMsg.goBack')}</a>
 </div>
 
