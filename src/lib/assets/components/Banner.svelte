4f5654fc9566ace2f53967eb1ebef43ab11eb4e7
Jan Biernacik <jan@biernacik.dev>
2025-08-18 14:52:04 +0200

    Fix social media link layout

--- src/lib/assets/components/Banner.svelte
+++ src/lib/assets/components/Banner.svelte
@@ -1,7 +1,7 @@
 <div class="flex justify-between items-center py-[var(--spacing-xx-large)] max-md:flex-col-reverse">
     <div>
         <h1 class="mb-[var(--spacing-x-small)]! md:text-6xl! max-md:text-center text-[var(--color-text)]!">Jan Biernacik</h1>
-        <div class="[&>a]:text-2xl md:[&>a]:mr-[var(--spacing-small)] max-md:[&>a]:mx-[var(--spacing-x-small)] [&>a]:hover:text-[var(--color-text)]! [&>a]:transition-all">
+        <div class="[&>a]:text-2xl md:[&>a]:mr-[var(--spacing-small)] max-md:[&>a]:mx-[var(--spacing-small)] [&>a]:hover:text-[var(--color-text)]! [&>a]:transition-all max-md:[&>a]:mr-0 max-md:[&>a]:text-center">
             <a href="https://github.com/janjbnck" aria-label="GitHub"><i class="bi bi-github"></i></a>
             <a href="https://x.com/janjbnck" aria-label="X"><i class="bi bi-twitter-x"></i></a>
             <a href="https://instagram.com/janjbnck" aria-label="Instagram"><i class="bi bi-instagram"></i></a>
