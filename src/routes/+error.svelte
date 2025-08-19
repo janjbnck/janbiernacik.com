65f853367ac510afb614cb91dd88b74fc75f59db
Jan Biernacik <jan@biernacik.dev>
2025-08-19 06:42:54 +0200

    Add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { page } from '$app/state';
+	import PageHeader from '$lib/assets/components/PageHeader.svelte';
+	import { t } from 'svelte-i18n';
+</script>
+
+<PageHeader pageTitle={page.status} />
+<p>{$t('specialMsg.errorOccurred')}: {page.error.message}</p>
